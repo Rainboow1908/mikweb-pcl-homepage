2e3bbbe86da9e0b630755a9d749ee15058f40ab0
MikWeb <pcl-homepage@mik.local>
2026-06-21 15:17:25 +0800

    refactor: clean install-startup.ps1, simplify bat to caller

--- install-startup.bat
+++ install-startup.bat
@@ -1,12 +1,4 @@
 @echo off
 :: MikWeb PCL - Add to Windows Startup (Run as Administrator)
-
-set "TARGET=%~dp0start.ps1"
-set "STARTUP=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
-
-powershell -Command "$ws = New-Object -ComObject WScript.Shell; $s = $ws.CreateShortcut('%STARTUP%\MikWeb-PCL.lnk'); $s.TargetPath = 'powershell.exe'; $s.Arguments = '-WindowStyle Hidden -File %TARGET%'; $s.WorkingDirectory = '%~dp0'; $s.Save()"
-
-echo [OK] Added to startup. Server will auto-start on next boot.
-echo.
-echo PCL URL: http://localhost:38080/pcl-homepage.xaml
+powershell -ExecutionPolicy Bypass -File "%~dp0install-startup.ps1"
 pause
