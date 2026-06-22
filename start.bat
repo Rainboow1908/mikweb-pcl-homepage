6c2c04f57b4cd748ebfdfd89a757689a9f47045d
MikWeb <pcl-homepage@mik.local>
2026-06-22 21:03:36 +0800

    smart progress bar: matches git pull duration, min 1s

--- start.bat
+++ start.bat
@@ -4,20 +4,36 @@ cd /d "%~dp0"
 
 git fetch 2>nul
 git status -uno 2>nul | find "Your branch is behind" >nul
-if %errorlevel% equ 0 (
-    echo An update is available.
-    choice /c yn /m "Apply update now?"
-    if errorlevel 2 goto skip
-    if errorlevel 1 (
-        set "bar="
-        for /l %%i in (1,1,5) do (
-            set "bar=!bar!#####"
-            set /p "=Updating [!bar!] "<nul
-            choice /t 1 /d y /c y >nul
-        )
-        echo.
-        git pull
-    )
+if %errorlevel% neq 0 (
+    start /min "" node server.js
+    exit /b
 )
+
+echo An update is available.
+choice /c yn /m "Apply update now?"
+if errorlevel 2 goto skip
+
+:: Start update in background
+set "flag=%temp%\mikweb-update-flag"
+start /b cmd /c "git pull >nul 2>&1 && echo 1>%flag%"
+set "bar="
+set "steps=0"
+
+:loop
+set /a "steps+=1"
+set "bar=!bar!#"
+set /p "=Updating [!bar!] "<nul
+choice /t 1 /d y /c y >nul 2>&1
+if exist "%flag%" goto done
+if %steps% lss 20 goto loop
+
+:done
+:: Pad to at least 1 second total
+if %steps% lss 1 (
+    choice /t 1 /d y /c y >nul 2>&1
+)
+del "%flag%" 2>nul
+echo.
+
 :skip
 start /min "" node server.js
