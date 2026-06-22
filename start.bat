1ba1ba9ac1e3848c3df5a52c5c2f0e83c4f622eb
MikWeb <pcl-homepage@mik.local>
2026-06-22 21:07:43 +0800

    ensure spinner runs at least 1 second

--- start.bat
+++ start.bat
@@ -14,6 +14,7 @@ choice /c yn /m "Apply update now?"
 if errorlevel 2 goto skip
 
 set "flag=%temp%\mikweb-update-flag"
+set "elapsed=0"
 start /b cmd /c "git pull >nul 2>&1 && echo 1>%flag%"
 set "spin=\|/-"
 set "i=0"
@@ -24,7 +25,11 @@ set /a "idx=i %% 4"
 call set "c=%%spin:~!idx!,1%%"
 set /p "=Updating... !c! "<nul
 choice /t 1 /d y /c y >nul 2>&1
-if exist "%flag%" goto done
+set /a "elapsed+=1"
+if exist "%flag%" (
+    if !elapsed! geq 1 goto done
+    goto spin
+)
 goto spin
 
 :done
